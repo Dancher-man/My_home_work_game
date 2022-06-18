9e7b20b9e8bf53a28f3a76eb3cd8a0d2d45bf263
Dancher-man <daniyar.usenov.2012@mail.ru>
2022-06-18 14:11:54 +0600

    create new file forms.html and urls.py with webapp and add with view.py function index_create

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_create(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, "forms.html")
+
+# def index_create(request):
+#     context = {}
+#     if request.method == "GET":
+#         return render(request, "create_index.html")
+#     else:
+#         if request.POST.get('choice') == 'add':
+#             query = int(request.POST.get('first_number')) + int(request.POST.get('second_number'))
+#             context = {
+#                 'result': f"Result: {request.POST.get('first_number')} + {request.POST.get('second_number')} = {query}"
+#             }
+#         elif request.POST.get('choice') == 'subtract':
+#             query = int(request.POST.get('first_number')) - int(request.POST.get('second_number'))
+#             context = {
+#                 'result': f"Result: {request.POST.get('first_number')} - {request.POST.get('second_number')} = {query}"
+#             }
+#         elif request.POST.get('choice') == 'multiply':
+#             query = int(request.POST.get('first_number')) * int(request.POST.get('second_number'))
+#             context = {
+#                 'result': f"Result: {request.POST.get('first_number')} * {request.POST.get('second_number')} = {query}"
+#             }
+#         elif request.POST.get('choice') == 'divide':
+#             query = int(request.POST.get('first_number')) / int(request.POST.get('second_number'))
+#             context = {
+#                 'result': f"Result: {request.POST.get('first_number')} / {request.POST.get('second_number')} = {query}"
+#             }
+#     return render(request, 'create_index.html', context)
